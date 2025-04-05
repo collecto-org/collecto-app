5432233ec61a4f2f80cc11a17f4ad9e624a3d3a6
Martin Rivas <martin.rivas.r@hotmail.com>
2025-04-05 00:00:44 -0600

    configuracion inicial del backend con conexcion a MongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,9 @@
-console.log("Collecto backend inicial");
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://collectokc:esMNd9R3meOaMVsm@cluster0.skl5wni.mongodb.net/collecto-dev?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('✅ Conectado a MongoDB correctamente');
+  })
+  .catch((error) => {
+    console.error('❌ Error al conectar a MongoDB:', error);
+  });
