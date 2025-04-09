c688eb831ccc1f7d30549f766ffde0f363e3de90
josemiguel736 <robertz@youremail.com>
2025-04-09 21:19:53 +0200

    feature/redux-tool-kit-connect-backend

--- Frontend/src/store/store.ts
+++ Frontend/src/store/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice"
+import { authApi } from "../services/authApi";
 
 
 export const store = configureStore({
     reducer:{
-        auth: authReducer
-    }
+        auth: authReducer,
+        [authApi.reducerPath]: authApi.reducer
+    },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>;
--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const apiUrl = import.meta.VITE_API_URL
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+ server:{
+  proxy:{
+    "/api":"http://localhost:3000",
+  }
+ }})
