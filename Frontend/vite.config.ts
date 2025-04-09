a4f32c7843e0520ba863936bc52142a7474f6f58
josemiguel736 <robertz@youremail.com>
2025-04-09 23:56:38 +0200

    refactor/peticiones-login-ok

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const apiUrl = import.meta.VITE_API_URL
+const apiUrl = process.env.VITE_API_URL;
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
  server:{
   proxy:{
-    "/api":"http://localhost:3000",
+    "/api":apiUrl || "http://localhost:3000",
   }
  }})
