e15b9b88d11d8ad85a407c56b7fcefd15daf83c9
Martin Rivas <martin.rivas.r@hotmail.com>
2025-04-19 17:16:29 -0600

    feat: se corrige el home page

--- Frontend/src/componentsUI/pages/develop/UniversePage.tsx
+++ Frontend/src/componentsUI/pages/develop/UniversePage.tsx
@@ -18,7 +18,7 @@ export default function UniversePage() {
   const {adverts,total} = useSelector((state: RootState) => selectFilterAdverts(state));//  obtener los anuncios y el total
   const { slug } = useParams()
 
-  const {adverts,total} = useSelector((state: RootState) => selectAdverts(state));//  obtener los anuncios y el total
+  //const {adverts,total} = useSelector((state: RootState) => selectAdverts(state));//  obtener los anuncios y el total
 
   const filter = useSelector((state:RootState)=> selectFilters(state)) //  obtener los valores del filtro 
   const { refetch  } = useFilterAdvertsQuery(filter); // Peticion para obtener adverts filtrados
@@ -39,42 +39,6 @@ export default function UniversePage() {
           logos={universeLogos}
         />
 
-
-      
-      <div className="grid grid-cols-12 gap-1 px-1 py-1">
-        <div className="col-span-12 md:col-span-3 flex  items-center  justify-center">
-            <Title 
-            headerLabel="Universo" 
-            label={slug || ""} 
-             />
-
-        </div>
-        
-        <div className="col-span-12 md:col-span-9  flex items-end  justify-center ">
-          <PaginationBlock
-            currentPage={currentPage}
-            totalPages={totalPages}
-            pageSize={pageSize}
-            onPageChange={setCurrentPage}
-            onPageSizeChange={(size) =>{
-              setPageSize(size);
-              setCurrentPage(1);
-            }}
-  
-          />
-        </div>
-        
-        <div className="col-span-12 md:col-span-3 border border-black">
-          <SideBarMenu 
-          title="Tipo de producto"
-          options={sideBarMenu}
-          />
-        </div>
-
-        <div className="col-span-12 md:col-span-9 border border-black">
-          <ProductGrid adverts={adverts} />
-        </div>
-      </div>
 
         <FilteredAdvertSectionProps
               headerLabel="Universo"
