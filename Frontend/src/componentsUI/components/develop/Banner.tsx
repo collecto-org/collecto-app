bed80b9939c41f5fd892f177dd3a0b690950ea2c
JohaDiaz <johadiazdev@gmail.com>
2025-04-19 10:35:30 +0200

    feat: actualización de recursos - banners, logos, favicon

--- Frontend/src/componentsUI/components/develop/Banner.tsx
+++ Frontend/src/componentsUI/components/develop/Banner.tsx
@@ -1,4 +1,4 @@
-import  { useMemo } from "react";
+import { useMemo } from "react";
 import ImageGrid from "../../components/develop/ImageGrid";
 import HighlightedText from "../../elements/HighlightedText";
 
@@ -10,33 +10,43 @@ interface BannerProps {
   height?: string;
 }
 
-
-export default function Banner({ backgroundImages, text, highlights, logos, height = "h-80" }: BannerProps) {
+export default function Banner({
+  backgroundImages,
+  text,
+  highlights,
+  logos,
+  height = "h-80",
+}: BannerProps) {
   const selectedImage = useMemo(() => {
-    const randomIndex = Math.floor(Math.random() * backgroundImages.length)
-    return backgroundImages[randomIndex]
-  },[backgroundImages])
+    const randomIndex = Math.floor(Math.random() * backgroundImages.length);
+    return backgroundImages[randomIndex];
+  }, [backgroundImages]);
   return (
-    <div className={`relative w-full ${height ?? "h-80"} rounded-md overflow-hidden`}>
+    <div
+      className={`relative w-full ${
+        height ?? "h-80"
+      } rounded-md overflow-hidden`}
+    >
       <img
         src={selectedImage}
         alt="Banner"
         className="absolute inset-0 w-full h-full object-cover z-0"
       />
 
-      <div className="relative z-10 w-full h-full flex flex-col justify-center items-center
-       bg-black bg-opacity-30 text-white text-center px-2 md:px-4 max-w-screen-xl mx-auto">
-
+      <div
+        className="relative z-10 w-full h-full flex flex-col justify-center items-center
+       bg-black bg-opacity-30 text-white text-center px-2 md:px-4 max-w-screen-xl mx-auto"
+      >
         <h2 className="text-xl md:text-xl font-semibold">
-              <HighlightedText text={text} highlights={highlights} />
+          <HighlightedText text={text} highlights={highlights} />
         </h2>
         <ImageGrid
-            logos={logos}
-            columns={4}
-            width={150}
-            height={75}
-            onClickLogo={(src) => alert(`Filtrar por universo: ${src}`)}
-          />
+          logos={logos}
+          columns={5}
+          width={160}
+          height={80}
+          onClickLogo={(src) => alert(`Filtrar por universo: ${src}`)}
+        />
       </div>
     </div>
   );
--- Frontend/src/componentsUI/containers/develop/Navbar.tsx
+++ Frontend/src/componentsUI/containers/develop/Navbar.tsx
@@ -1,10 +1,10 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import Logo from "../../elements/Logo";
 import SearchBar from "../../components/develop/SearchBar";
 import LanguajeSelector from "../../components/develop/LanguajeSelector";
 import UserMenu from "../../components/develop/UserMenu";
 import NavActions from "../../components/develop/NavActions";
-import NavIcons from "../../components/develop/NavIcons"
+import NavIcons from "../../components/develop/NavIcons";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
 import { selectUser } from "@/store/selectors/userSelectors";
@@ -13,31 +13,33 @@ export default function Navbar() {
   const [query, setQuery] = useState("");
   const user = useSelector((state: RootState) => selectUser(state));
 
-
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 bg-cream text-darkblue shadow-md flex justify-between items-center py-0 px-4">
-      
-        <div className="flex items-end p-2">
-          <Logo src="/logos/logo-horizontal-collecto.png" alt="Collecto logo" width={100} height={15} redirectTo="/"/>
-        </div>
-        <div className="flex-1 max-w-lg">
-          <SearchBar
+    <nav className="fixed top-0 left-0 w-full z-50 bg-white text-darkblue shadow-md flex justify-between items-center py-0 px-4">
+      <div className="flex items-end p-2">
+        <Logo
+          src="/logos/logo-horizontal-collecto.png"
+          alt="Collecto logo"
+          width={150}
+          height={25}
+          redirectTo="/"
+        />
+      </div>
+      <div className="flex-1 max-w-lg">
+        <SearchBar
           placeholder="Busca en todos los universos"
           value={query}
           onChange={setQuery}
           onSearch={() => console.log("Buscar en navbar:", query)}
           width="w-full"
-          />
-        </div>
-        <div className="flex items-center">
-          <NavIcons user={user}/>
-          <NavActions user={user} />
-          <UserMenu user={user}/>
-          <LanguajeSelector/>
-        </div>
-      
+        />
+      </div>
+      <div className="flex items-center">
+        <NavIcons user={user} />
+        <NavActions user={user} />
+        <UserMenu user={user} />
+        <LanguajeSelector />
+      </div>
     </nav>
-
   );
 }
 
