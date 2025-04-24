b9c7f8fe84a4c504519bcbb6b0bded03cd3ee19a
JohaDiaz <johadiazdev@gmail.com>
2025-04-24 07:41:02 +0200

    Creadas secciones Mis anuncios y Favoritos en Userprofile

--- Frontend/src/componentsUI/pages/develop/UserAdvertsPublished.tsx
+++ Frontend/src/componentsUI/pages/develop/UserAdvertsPublished.tsx
@@ -1,44 +1,50 @@
-import { useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import { useGetMyadvertsQuery } from "@/services/usersApi";
 import { FilterAdverts } from "@/services/schemas/AdvertsSchemas";
-import {
-  selectFilters,
-} from "@/store/selectors/advertsSelectors";
+import { selectFilters } from "@/store/selectors/advertsSelectors";
 
 import FilteredAdvertSectionProps from "@/componentsUI/containers/develop/FilteredAdverSection";
 import NoResults from "@/componentsUI/elements/noResults";
 
 export default function UserAdvertsPublished() {
-
   const filters = useSelector(selectFilters);
 
   const filterProducts: FilterAdverts = {
-    ...filters
+    ...filters,
   };
 
-  const {data:adverts, isLoading, isError } = useGetMyadvertsQuery(filterProducts);
+  const {
+    data: adverts,
+    isLoading,
+    isError,
+  } = useGetMyadvertsQuery(filterProducts);
 
-  if(isLoading) return <p>Loading...</p>
-  if(isError) return <p>Ocurrio un error</p>
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Ocurrio un error</p>;
   return (
     <>
       <div className="pt-24 md:pt-32">
         <div className="max-w-7xl mx-auto px-4 space-y-6">
           {/* Encabezado */}
           <div className="text-center">
-            <h1 className="text-2xl font-bold text-darkblue">Tus favoritos</h1>
+            <h1 className="text-2xl font-bold text-darkblue">
+              Tus Artículos Publicados
+            </h1>
             <p className="text-sm text-gray-600">
-              Aquí están los anuncios que has marcado como favoritos.
+              Aquí encontrarás tus anuncios publicados.
             </p>
           </div>
 
           {/* Contenido */}
-          {adverts ?
-          <FilteredAdvertSectionProps
-            headerLabel="Mis productos"
-            label="Tus anuncios publicados"
-            adverts={adverts.adverts}
-          /> : <NoResults/>}
+          {adverts ? (
+            <FilteredAdvertSectionProps
+              headerLabel="Mis productos"
+              label="Tus anuncios publicados"
+              adverts={adverts.adverts}
+            />
+          ) : (
+            <NoResults />
+          )}
         </div>
       </div>
     </>
